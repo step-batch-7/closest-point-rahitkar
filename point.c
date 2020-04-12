c60e82e72fb8f04ecce0f294c1c92b47d387cad6
Rahit <rahitkar@users.noreply.github.com>
2020-04-12 19:35:40 +0530

    added point.c file

---
+++ point.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include "point.h"
+
+void get_closest_food(Point food_points[], int points_length, Point current_location, Point *closest_food_location) {
+  printf("hello");
+}
