0ab7fa6d3e85ad510c18559b7f2d00ddd59e811e
Rahit <rahitkar@users.noreply.github.com>
2020-04-12 19:47:34 +0530

    modified main.c for properly calling of get_closest_food function and printing of desired result

--- main.c
+++ main.c
@@ -1,6 +1,17 @@
+#include <stdio.h>
 #include "point.h"
 
 int main(void)
 {
+  Point food_points[5] = {{18, 76}, {19, 66}, {89, 57}, {9, 71}, {55, 38}};
+  int points_length = 5;
+  Point current_location[] = {{18, 86}, {97, 27}, {69, 7}, {10, 94}, {36, 27}};
+  Point closest_food_location = {0, 0};
+
+  for (int i = 0; i < 5; i++)
+  {
+    get_closest_food(food_points, points_length, current_location[i], &closest_food_location);
+    printf("Closest food : [%d %d] of organism: [%d %d]\n", closest_food_location.x, closest_food_location.y, current_location[i].x, current_location[i].y);
+  }
   return 0;
 }
